2de23d29f9d41effd390d1e195c285d283f26b85
Karan Vishwakarma <karanvishwakarma3007@gmail.com>
2026-02-02 15:51:27 +0530

    Added the cart page app

--- carts/admin.py
+++ carts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cart, CartItem
+from django.urls import path, include
 
 # Register your models here.
+
+admin.site.register(Cart)
+admin.site.register(CartItem)
